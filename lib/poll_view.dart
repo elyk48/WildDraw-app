e6d5de39d16f41d036427353ce41cd84371d58f2
elyeskabous <elyes.kabous@gmail.com>
2021-12-02 04:47:31 +0100

    Week 4 : Documentation for poll view and vote

--- lib/poll_view.dart
+++ lib/poll_view.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+/// main class for displaying polls for users to vote
 class MyPollDisplay extends StatefulWidget {
   @override
   _MyPollDisplayState createState() => _MyPollDisplayState();
@@ -44,17 +44,25 @@ class _MyPollDisplayState extends State<MyPollDisplay> {
           ),
         ],
       ),
+      ///stream builder used for the stream of data
+      ///widget based on the result of a Stream of data collected
       body: StreamBuilder(
+        ///getting the stream of data from firebase store
           stream: FirebaseFirestore.instance.collection('poll').snapshots(),
           builder:(BuildContext context,AsyncSnapshot snapshot){
+            ///testing if the snapshot has data
             if(!snapshot.hasData){
               return LinearProgressIndicator();
             }
+            ///if it has data it rturn a listview builder which contains the polls
             else{
               return ListView.builder(
+                ///the number of items in the snapshot..
                   itemCount: snapshot.data.docs.length,
                   itemBuilder: (context,index){
+                    ///getting a single poll using the index..
                     DocumentSnapshot mypoll=snapshot.data.docs[index];
+                    ///geting the poll question..
                     String question=mypoll['question'];
                     return Container(
                       margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
@@ -72,17 +80,23 @@ class _MyPollDisplayState extends State<MyPollDisplay> {
                             borderRadius: BorderRadius.circular(5),
 
                           ),
+                          ///puting the question queried in a text widget
                           child: Text("$question",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),),
                         ),
                         Container(
-                          height: 74.0*mypoll['length'],                                          child: StreamBuilder(
-
+                          height: 74.0*mypoll['length'],
+                          ///using another Streambuilder for the options
+                          child: StreamBuilder(
+                            ///stream builder used for the stream of data
+                            ///widget based on the result of a Stream of data collected
                             stream: FirebaseFirestore.instance.collection('poll').doc('$question').collection('options').snapshots(),
+                            ///passing the context and the snapshot
                             builder: (BuildContext context,AsyncSnapshot snapshot){
-
+                               ///testing if the snapshot has data or not
                               if(!snapshot.hasData)
                                 return LinearProgressIndicator();
                               else{
+                                ///if yes it returns a listview that has the options data
                                 return ListView.builder(
                                     physics: new NeverScrollableScrollPhysics(),
                                     itemCount: snapshot.data.docs.length,
@@ -106,27 +120,34 @@ class _MyPollDisplayState extends State<MyPollDisplay> {
 
                                               title: Text(myoptions['name']),
                                               trailing: Text(myoptions['votes'].toString(),style: TextStyle(fontWeight: FontWeight.bold),),
+                                              ///voting part (beta version)...
                                               onTap: (){
                                                 String uid;
                                                 int i=0;
+                                                ///getting the currentuser uid
 
                                                 uid=FirebaseAuth.instance.currentUser!.uid;
 
                                                   print(uid);
-
+                                                   ///getting the document to set the vote of the user
                                                   var doc=FirebaseFirestore.instance.collection('poll').doc(question).collection('users').doc(uid);
                                                   doc.get().then((value){
+                                                    ///testing if the user already voted
                                                     if(value.exists){
 
                                                       i=1;
                                                       print("$i");
-                                                    }else{
+                                                    }
+                                                    ///if its not the case
+                                                    else{
                                                       String qw=myoptions.id;
                                                       var doc2=FirebaseFirestore.instance.collection('poll').doc(question).collection('options').doc(qw);
                                                       print("qw");
+                                                      ///updating the votes
                                                       doc2.update({'votes': 1+myoptions['votes']});
                                                       i=2;
                                                       print("$i");
+                                                      ///setting the voted true for the currentuser and printing done when the task is completed
                                                       FirebaseFirestore.instance.collection('poll').doc(question).collection('users').doc(uid).set({
                                                         "votes":true,
                                                       }).whenComplete((){print("done");});
@@ -152,10 +173,11 @@ class _MyPollDisplayState extends State<MyPollDisplay> {
           }
       ),
 
-
+            /// a button that takes the admin where he can add a new poll....
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add,),
           onPressed:(){
+            ///using the navigator to point the context to create poll route
             Navigator.pushNamed(context,'/pollCreate');
           }
       ),
